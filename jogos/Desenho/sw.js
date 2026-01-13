7954042c4e972cfb1e367e77e44b9b7a1a76736e
Ailton Akio Nakata <akionkt@gmail.com>
2026-01-13 19:35:44 -0300

    Desenho - Atualizado

---
+++ jogos/Desenho/sw.js
@@ -0,0 +1,20 @@
+const CACHE = "traco-unico-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
